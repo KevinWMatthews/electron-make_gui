8152067f211a6d6233b1e02bc741c9d7b7e3f37e
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-09 13:25:27 -0700

    Set button text to user's selection

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const {dialog} = require('electron').remote;
 
 let btn_select_source = document.getElementById('btn_select_source');
 btn_select_source.addEventListener('click', (event) => {
-  dialog.showOpenDialog({
+  let source_dir = dialog.showOpenDialog({
     properties: ['openDirectory', 'multiSelections']
   });
+  event.target.innerHTML = source_dir;
 });
