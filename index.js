812095866b7ebe904b51f8a4813c6442390afc9d
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-11 09:00:09 -0700

    Launch 'ls' system command on button press.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const {dialog} = require('electron').remote;
+const {spawn} = require('child_process');
 
 // Wait until DOM is ready?
+
+let project_root;
 
 let btn_select_source = document.getElementById('btn_select_source');
 btn_select_source.addEventListener('click', (event) => {
@@ -8,4 +11,23 @@ btn_select_source.addEventListener('click', (event) => {
     properties: ['openDirectory', 'multiSelections']
   });
   event.target.innerHTML = source_dir;
+  project_root = source_dir;
 });
+
+let btn_test = document.getElementById('btn_test');
+btn_test.addEventListener('click', (event) => {
+  if (project_root) {   //TODO look into this conditional!!
+    // What if this takes a long time to execute? Will we lose scope?
+    const ls = spawn('ls', ['-lh', project_root]);
+
+    ls.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    ls.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    })
+    ls.on('close', (exit_code) => {
+      console.log(`Exited with code: ${exit_code}`);
+    })
+  }
+})
