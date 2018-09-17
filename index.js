61100bac78ca80a650394ec86e4e944145b6303c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-16 21:40:14 -0700

    Print command output to HTML <pre>

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const {spawn} = require('child_process');
 
 let project_root;
 
+// Testing only, because I'm too lazy to figure out the right way to test this.
+project_root = '/home/kevin/coding/c/build_terminate_program';
+document.getElementById('btn_select_source').innerHTML = project_root;
+
+
 let btn_select_source = document.getElementById('btn_select_source');
 btn_select_source.addEventListener('click', (event) => {
   let dialog_properties = {
@@ -22,17 +27,24 @@ let btn_clean = document.getElementById('btn_clean');
 btn_clean.addEventListener('click', (event) => {
   if (project_root) {   //TODO look into this conditional!!
     // What if this takes a long time to execute? Will we lose scope?
+    let command = 'make';
+    let options = ['clean'];
     const make_clean = spawn('make', ['clean'], {'cwd': project_root});
 
+    let output = document.getElementById('pre_output');
+    output.innerHTML = `Executing command: ${command} ${options}\n`;
     make_clean.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+      output.innerHTML += data;
     });
     make_clean.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    })
+      output.innerHTML += data;
+    });
+    make_clean.on('error', (err) => {
+      output.innerHTML += `Error: ${err.message}\n`;
+    });
     make_clean.on('close', (exit_code) => {
-      console.log(`Exited with code: ${exit_code}`);
-    })
+      output.innerHTML += `Exited with code: ${exit_code}\n`;
+    });
   }
 });
 
@@ -40,33 +52,49 @@ let btn_compile = document.getElementById('btn_compile');
 btn_compile.addEventListener('click', (event) => {
   if (project_root) {   //TODO look into this conditional!!
     // What if this takes a long time to execute? Will we lose scope?
-    const make_all = spawn('make', [], {'cwd': project_root});
+    let command = 'make';
+    let options = [];
+    const make_all = spawn(command, options, {'cwd': project_root});
 
+    let output = document.getElementById('pre_output');
+    output.innerHTML = `Executing command: ${command} ${options}\n`;
     make_all.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+      output.innerHTML += data;
     });
     make_all.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    })
+      output.innerHTML += data;
+    });
+    make_all.on('error', (err) => {
+      output.innerHTML += `Error: ${err.message}\n`;
+    });
     make_all.on('close', (exit_code) => {
-      console.log(`Exited with code: ${exit_code}`);
-    })
+      output.innerHTML += `Exited with code: ${exit_code}\n`;
+    });
   }
 });
 
 let btn_run = document.getElementById('btn_run');
 btn_run.addEventListener('click', (event) => {
   if (project_root) {
-    const run_program = spawn('./bin/terminate_forked', {'cwd': project_root});
+    let command = './bin/terminate_forked';
+    let options = [];
+    const run_program = spawn(command, options, {'cwd': project_root});
 
+    let output = document.getElementById('pre_output');
+    output.innerHTML = `Executing command: ${command} ${options}\n`;
     run_program.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+      output.innerHTML += data;
     });
     run_program.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
+      output.innerHTML += data;
+    });
+    run_program.on('error', (err) => {
+      output.innerHTML += `Error: ${err.message}\n`;
     });
     run_program.on('close', (exit_code) => {
-      console.log(`Exited with code: ${exit_code}`);
+      if (exit_code != 0) {
+        output.innerHTML += `Exited with code: ${exit_code}\n`;
+      }
     });
   }
 });
