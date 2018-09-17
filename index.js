9e6ca8cb13ad354d436678dd4e482dda8ac0bf99
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-09-16 21:46:45 -0700

    Extract common printing behavior into function
    All buttons print the same way, at the moment.

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ let project_root;
 project_root = '/home/kevin/coding/c/build_terminate_program';
 document.getElementById('btn_select_source').innerHTML = project_root;
 
+// HTML element must respond to the innerHTML() method.
+function printOutputToHtml(child_process, element) {
+  child_process.stdout.on('data', (data) => {
+    element.innerHTML += data;
+  });
+  child_process.stderr.on('data', (data) => {
+    element.innerHTML += data;
+  });
+  child_process.on('error', (err) => {
+    element.innerHTML += `Error: ${err.message}\n`;
+  });
+  child_process.on('close', (exit_code) => {
+    element.innerHTML += `Exited with code: ${exit_code}\n`;
+  });
+}
+
 
 let btn_select_source = document.getElementById('btn_select_source');
 btn_select_source.addEventListener('click', (event) => {
@@ -29,22 +45,11 @@ btn_clean.addEventListener('click', (event) => {
     // What if this takes a long time to execute? Will we lose scope?
     let command = 'make';
     let options = ['clean'];
-    const make_clean = spawn('make', ['clean'], {'cwd': project_root});
-
     let output = document.getElementById('pre_output');
     output.innerHTML = `Executing command: ${command} ${options}\n`;
-    make_clean.stdout.on('data', (data) => {
-      output.innerHTML += data;
-    });
-    make_clean.stderr.on('data', (data) => {
-      output.innerHTML += data;
-    });
-    make_clean.on('error', (err) => {
-      output.innerHTML += `Error: ${err.message}\n`;
-    });
-    make_clean.on('close', (exit_code) => {
-      output.innerHTML += `Exited with code: ${exit_code}\n`;
-    });
+
+    const child = spawn('make', ['clean'], {'cwd': project_root});
+    printOutputToHtml(child, output);
   }
 });
 
@@ -54,22 +59,11 @@ btn_compile.addEventListener('click', (event) => {
     // What if this takes a long time to execute? Will we lose scope?
     let command = 'make';
     let options = [];
-    const make_all = spawn(command, options, {'cwd': project_root});
-
     let output = document.getElementById('pre_output');
     output.innerHTML = `Executing command: ${command} ${options}\n`;
-    make_all.stdout.on('data', (data) => {
-      output.innerHTML += data;
-    });
-    make_all.stderr.on('data', (data) => {
-      output.innerHTML += data;
-    });
-    make_all.on('error', (err) => {
-      output.innerHTML += `Error: ${err.message}\n`;
-    });
-    make_all.on('close', (exit_code) => {
-      output.innerHTML += `Exited with code: ${exit_code}\n`;
-    });
+
+    const child = spawn(command, options, {'cwd': project_root});
+    printOutputToHtml(child, output);
   }
 });
 
@@ -78,23 +72,10 @@ btn_run.addEventListener('click', (event) => {
   if (project_root) {
     let command = './bin/terminate_forked';
     let options = [];
-    const run_program = spawn(command, options, {'cwd': project_root});
-
     let output = document.getElementById('pre_output');
     output.innerHTML = `Executing command: ${command} ${options}\n`;
-    run_program.stdout.on('data', (data) => {
-      output.innerHTML += data;
-    });
-    run_program.stderr.on('data', (data) => {
-      output.innerHTML += data;
-    });
-    run_program.on('error', (err) => {
-      output.innerHTML += `Error: ${err.message}\n`;
-    });
-    run_program.on('close', (exit_code) => {
-      if (exit_code != 0) {
-        output.innerHTML += `Exited with code: ${exit_code}\n`;
-      }
-    });
+
+    const child = spawn(command, options, {'cwd': project_root});
+    printOutputToHtml(child, output);
   }
 });
